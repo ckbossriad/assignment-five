7fdc04d126b453a0937aa240a7b2e8a6a7da8420
ckbossriad <rakibulhassanriad06@gmail.com>
2025-08-29 20:57:49 +0600

    make love count with js

---
+++ script.js
@@ -0,0 +1,119 @@
+// Increase Heart Count >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    // National Emergency Number
+    document.getElementById('national_heart').addEventListener("click", function(){
+          // console.log("Done")
+    
+          // Change The heart Color>>>
+      document.getElementById("national_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+          // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Police Helpline Number
+    document.getElementById('police_heart').addEventListener("click", function(){
+          // console.log("Done")
+    
+          // Change The heart Color>>>
+      document.getElementById("police_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+          // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Fire Service Number
+    document.getElementById('fire_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("fire_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Ambulance Service
+    document.getElementById('ambulance_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("ambulance_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Women And Child Helpline
+    document.getElementById('women_and_child_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("women_and_child_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Anti-Corruption Helpline
+    document.getElementById('curruption_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("curruption_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Electricity 
+    document.getElementById('electricity_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("electricity_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Brac Helpline
+    document.getElementById('brac_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("brac_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
+    // Bangladesh Railway Helpline
+    document.getElementById('railway_heart').addEventListener("click", function(){
+      // console.log("Done")
+    
+      // Change The heart Color>>>
+      document.getElementById("railway_heart").classList.add("text-red-500")
+    
+      const heart = document.getElementById('heart');
+      const heartInnerText = parseInt(heart.innerText);
+      const newHeartValue = heartInnerText + 1 ;
+      // console.log(newHeartValue)
+      document.getElementById('heart').innerText = newHeartValue
+    })
